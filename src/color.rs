fbcaf5a3259970679789cf4e9f0e628c7d8ab25f
Utku Boduroglu <utkubod@gmail.com>
2023-03-25 17:27:38 +0300

    chore: write_color cleanup

--- src/color.rs
+++ src/color.rs
@@ -4,12 +4,10 @@ use nalgebra::Vector3;
 pub type color3 = Vector3<f32>;
 
 pub fn write_color(pixel_color: color3, scales_per_pixel: u32) -> image::Rgb<u8> {
-    // TODO: the page states that the following gamma fix (gamma=2.0) would
-    // make the image brighter, but it makes it darker; though fixing shadow
-    // acne actually did make it brighter!?
-    let scale = 1.0/(scales_per_pixel as f32);
-    let corrected_color = pixel_color
-        .map(|p: f32| f32::sqrt(scale * p));
+    let scale = 1.0 / (scales_per_pixel as f32);
+
+    // x^(1/gamma) for gamma correction (gamma=2.0)
+    let corrected_color = pixel_color.map(|p: f32| f32::sqrt(scale * p));
 
     let r = corrected_color[0];
     let g = corrected_color[1];
